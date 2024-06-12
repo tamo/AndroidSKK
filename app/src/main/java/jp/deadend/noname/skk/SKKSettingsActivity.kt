3f806cd1319275ac83ddb5982c6967876ef6c4ca
Tamotsu Takahashi <ttakah+github@gmail.com>
2024-06-12 22:25:02 +0900

    使っていない import を削除

--- app/src/main/java/jp/deadend/noname/skk/SKKSettingsActivity.kt
+++ app/src/main/java/jp/deadend/noname/skk/SKKSettingsActivity.kt
@@ -3,7 +3,6 @@ package jp.deadend.noname.skk
 import android.content.Intent
 import android.os.Bundle
 import android.view.MenuItem
-import androidx.activity.addCallback
 import androidx.appcompat.app.AppCompatActivity
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
