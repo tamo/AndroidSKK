1959fa54e75adebdaca714d3233b58c9d29202eb
Tamotsu Takahashi <ttakah+github@gmail.com>
2025-11-16 15:28:50 +0900

    Remove special-case from keyPaddingBottom

--- app/src/main/java/jp/deadend/noname/skk/SKKPrefs.kt
+++ app/src/main/java/jp/deadend/noname/skk/SKKPrefs.kt
@@ -1,7 +1,6 @@
 package jp.deadend.noname.skk
 
 import android.content.Context
-import android.os.Build
 import androidx.core.content.edit
 import androidx.preference.PreferenceManager
 import kotlin.math.max
@@ -226,10 +225,7 @@ class SKKPrefs(context: Context) {
         }
 
     var keyPaddingBottom: Int
-        get() = prefs.getInt(
-            res.getString(R.string.pref_key_padding_bottom),
-            if (Build.VERSION.SDK_INT >= 35) 15 else 0
-        )
+        get() = prefs.getInt(res.getString(R.string.pref_key_padding_bottom), 0)
         set(value) = prefs.edit {
             putInt(res.getString(R.string.pref_key_padding_bottom), value)
         }
