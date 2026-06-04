tamo/AndroidSKK

commit d5e92f1c6709dde7916f13f1e00a3d43ee3c1800

Support back gestures
- Implement `OnBackInvokedCallback` (API 34+)
- Fix dimension calculations to handle display cutouts and system gestures
  - Add skkPrefs.gestureInsets to toggle the space for gestures
- Refactor `CandidatesViewContainer` touch handling
  - Add haptic and visual feedback when saving drag results
- Move bottom padding responsibility to SKKService
- Remove `handleBackKey` and do `handleCancel` on back gesture

app/src/main/java/jp/deadend/noname/skk/engine/SKKEngine.kt
@@ -154,28 +154,6 @@ class SKKEngine(
         changeState(SKKHanKanaState)
     }
 
-    // Backspace ではなくジェスチャー等で「戻る」操作
-    internal fun handleBackKey(): Boolean {
-        if (mRegister.isOngoing) {
-            mRegister.mStack.removeFirst()
-        }
-
-        return when {
-            state.isTransient -> {
-                reset() // 確定なし
-                changeState(kanaState)
-                true
-            }
-
-            mRegister.isOngoing -> {
-                reset()
-                true
-            }
-
-            else -> false
-        }
-    }
-
     internal fun handleEnter(): Boolean {
         when {
             state.hasCandidates -> mCandidates.pickCandidate(mCandidates.mIndex)
@@ -802,7 +780,7 @@ class SKKEngine(
     internal fun changeSoftKeyboard(state: SKKState) {
         // 仮名からASCII以外の一時的なキーボードになるときや明示的変更のとき記録して後で戻れるようにしておく
         when (state) {
-            SKKAbbrevState, SKKZenkakuState -> cameFromFlick = mService.isFlickWidth
+            SKKAbbrevState, SKKZenkakuState -> cameFromFlick = mService.isFlickWidth()
             SKKASCIIState -> cameFromFlick = false
             SKKHiraganaState,
             SKKKatakanaState, SKKHanKanaState -> cameFromFlick = true

app/src/main/AndroidManifest.xml
@@ -17,6 +17,7 @@
     <application
         android:name=".SKKApplication"
         android:allowBackup="true"
+        android:enableOnBackInvokedCallback="true"
         android:icon="@drawable/icon"
         android:label="@string/ime_name"
         android:supportsRtl="false">

app/src/main/java/jp/deadend/noname/skk/AbbrevKeyboardView.kt
@@ -16,7 +16,7 @@ class AbbrevKeyboardView : KeyboardView, KeyboardView.OnKeyboardActionListener {
 
     override fun setService(service: SKKService) {
         super.setService(service)
-        keyboard = Keyboard(context, R.xml.abbrev, mService.mScreenWidth, mService.mScreenHeight)
+        keyboard = Keyboard(context, R.xml.abbrev, mService.mRootWidth, mService.mScreenHeight)
         onKeyboardActionListener = this
         setKeyState(SKKAbbrevState)
     }

app/src/main/java/jp/deadend/noname/skk/CandidatesViewContainer.kt
@@ -19,9 +19,13 @@ package jp.deadend.noname.skk
 import android.content.Context
 import android.content.res.Configuration
 import android.util.AttributeSet
+import android.util.SparseArray
 import android.view.MotionEvent
-import android.widget.FrameLayout
 import android.widget.LinearLayout
+import androidx.core.content.res.ResourcesCompat
+import androidx.core.graphics.drawable.toDrawable
+import androidx.core.util.isEmpty
+import androidx.core.util.size
 import jp.deadend.noname.skk.databinding.ViewCandidatesBinding
 import kotlin.math.abs
 
@@ -37,7 +41,7 @@ class CandidatesViewContainer(screen: Context, attrs: AttributeSet) : LinearLayo
     internal val minHeight = resources.getDimensionPixelSize(R.dimen.candidates_scroll_button_width)
     private lateinit var mService: SKKService
 
-    private val mActivePointers = mutableListOf<Pair<Int, Float>>()
+    private val mActivePointers = SparseArray<Float>()
     private var mDragging = false
     private var mDragStartLeft = 0
     private var mDragStartX = 0f
@@ -58,8 +62,17 @@ class CandidatesViewContainer(screen: Context, attrs: AttributeSet) : LinearLayo
         val onTouchListener = OnTouchListener { view, event ->
             try {
                 val id = event.getPointerId(event.actionIndex)
-                val idx = mActivePointers.indexOfFirst { it.first == id }
                 val x = event.getRawX(event.findPointerIndex(id))
+                when (event.actionMasked) {
+                    MotionEvent.ACTION_DOWN, MotionEvent.ACTION_POINTER_DOWN ->
+                        mActivePointers.put(id, x)
+
+                    MotionEvent.ACTION_MOVE -> for (i in 0 until event.pointerCount)
+                        mActivePointers.put(event.getPointerId(i), event.getRawX(i))
+
+                    MotionEvent.ACTION_POINTER_UP, MotionEvent.ACTION_UP ->
+                        mActivePointers.remove(id)
+                }
 
                 // y が本当に自分の範囲内か確認
                 event.setLocation(event.x, mCoordinates.let {
@@ -68,58 +81,55 @@ class CandidatesViewContainer(screen: Context, attrs: AttributeSet) : LinearLayo
                 })
                 if (event.y < 0 || height < event.y) return@OnTouchListener false
 
-                when (event.action and MotionEvent.ACTION_MASK) {
+                when (event.actionMasked) {
                     MotionEvent.ACTION_DOWN, MotionEvent.ACTION_POINTER_DOWN -> {
-                        mActivePointers.add(id to x)
                         if (!view.performClick()) {
                             mDragStartLeft = mService.leftOffset
-                            if (mActivePointers.count() == 1) {
+                            if (mActivePointers.size == 1) {
                                 mDragStartX = x
                                 mDragging = true
                             } else {
                                 mPinchStartWidth = width
                                 mPinchStartDistance = abs(
-                                    mActivePointers.last().second - mActivePointers.first().second
+                                    mActivePointers.valueAt(1) - mActivePointers.valueAt(0)
                                 )
                             }
                         }
                     }
 
                     MotionEvent.ACTION_MOVE -> {
-                        assert(idx != -1)
-                        mActivePointers[idx] = id to x
-                        if (mActivePointers.count() > 1) { // 幅の調整
+                        val rightEdge = mService.mInsets.left + mService.mRootWidth
+                        if (mActivePointers.size > 1) { // 幅の調整
                             val currentDistance = abs(
-                                mActivePointers.last().second - mActivePointers.first().second
+                                mActivePointers.valueAt(1) - mActivePointers.valueAt(0)
                             )
                             val newWidth =
                                 (mPinchStartWidth - mPinchStartDistance + currentDistance)
                                     .toInt().coerceIn(
                                         resources.getDimensionPixelSize(R.dimen.keyboard_minimum_width),
-                                        mService.mScreenWidth
+                                        mService.mRootWidth
                                     )
                             val newLeftOffset =
                                 mDragStartLeft + (mPinchStartDistance - currentDistance).toInt() / 2
                             mService.leftOffset =
-                                newLeftOffset.coerceIn(0, mService.mScreenWidth - newWidth)
+                                newLeftOffset.coerceIn(mService.mInsets.left, rightEdge - newWidth)
                             mService.inputViewWidth = newWidth
                         } else if (mDragging) { // 位置の調整
-                            val newLeftOffset = mDragStartLeft + (
-                                    mActivePointers.first().second - mDragStartX
-                                    ).toInt()
-                            mService.leftOffset =
-                                newLeftOffset.coerceIn(0, mService.mScreenWidth - width)
+                            val newLeftOffset =
+                                mDragStartLeft + (mActivePointers.get(id) - mDragStartX).toInt()
+                            mService.leftOffset = newLeftOffset.coerceIn(
+                                mService.mInsets.left, rightEdge - mService.inputViewWidth
+                            )
                             mService.setInputView(null)
                         }
                     }
 
                     MotionEvent.ACTION_POINTER_UP, MotionEvent.ACTION_UP -> {
-                        mActivePointers.removeAt(idx)
-                        if (mActivePointers.count() == 1) {
+                        if (mActivePointers.size == 1) {
                             saveWidth()
                             // もう一度ドラッグに戻る
                             mDragStartLeft = mService.leftOffset
-                            mDragStartX = mActivePointers.first().second
+                            mDragStartX = mActivePointers.valueAt(0)
                         } else if (mActivePointers.isEmpty()) {
                             if (mDragging) {
                                 if (mDragStartLeft != mService.leftOffset) {
@@ -129,6 +139,12 @@ class CandidatesViewContainer(screen: Context, attrs: AttributeSet) : LinearLayo
                             }
                             // DOWN で performClick() しているので else は無視
                         }
+                        // 正常終了し save されたことを伝えたい
+                        performHapticFeedback(skkPrefs.haptic)
+                        foreground =
+                            ResourcesCompat.getColor(resources, R.color.key_checked_color, null)
+                                .toDrawable()
+                        postDelayed({ foreground = null }, 100)
                     }
 
                     else -> dLog(event.toString())
@@ -144,9 +160,10 @@ class CandidatesViewContainer(screen: Context, attrs: AttributeSet) : LinearLayo
     }
 
     private fun saveLeft(leftOffset: Int) {
+        val currentWidth = mService.inputViewWidth
         val centerRate =
-            if (mService.mScreenWidth <= width) 0.5f
-            else (leftOffset * 2 + width) / 2.0f / mService.mScreenWidth
+            if (mService.mRootWidth <= currentWidth) 0.5f
+            else (leftOffset - mService.mInsets.left + currentWidth / 2.0f) / mService.mRootWidth
         when (resources.configuration.orientation) {
             Configuration.ORIENTATION_LANDSCAPE -> skkPrefs.keyCenterLand = centerRate
             else -> skkPrefs.keyCenterPort = centerRate
@@ -156,12 +173,13 @@ class CandidatesViewContainer(screen: Context, attrs: AttributeSet) : LinearLayo
     private fun saveWidth() {
         setSize(-1)
         val widthToSave = width * 100 /
-                if (mService.isFlickWidth) 100 else skkPrefs.keyWidthQwertyZoom
+                if (mService.isFlickWidth()) 100 else skkPrefs.keyWidthQwertyZoom
+        val minWidth = resources.getDimensionPixelSize(R.dimen.keyboard_minimum_width)
         when (resources.configuration.orientation) {
             Configuration.ORIENTATION_LANDSCAPE ->
-                skkPrefs.keyWidthLand = widthToSave.coerceIn(101, mService.mScreenWidth)
+                skkPrefs.keyWidthLand = widthToSave.coerceIn(minWidth, mService.mRootWidth)
 
-            else -> skkPrefs.keyWidthPort = widthToSave.coerceIn(101, mService.mScreenWidth)
+            else -> skkPrefs.keyWidthPort = widthToSave.coerceIn(minWidth, mService.mRootWidth)
         }
     }
 
@@ -178,14 +196,9 @@ class CandidatesViewContainer(screen: Context, attrs: AttributeSet) : LinearLayo
         if (px > 0) {
             binding.candidates.setTextSize(px)
         }
-        val width = mService.inputViewWidth - minHeight * 2 // minHeight == size of square button
         val height = if (lines > 0) lines * binding.candidates.mLineHeight else minHeight
-        val newLayoutParams = LayoutParams(width, height)
-        if (binding.frame.layoutParams.width != newLayoutParams.width ||
-            binding.frame.layoutParams.height != newLayoutParams.height
-        ) {
-            binding.frame.layoutParams = newLayoutParams
-            binding.candidates.layoutParams = FrameLayout.LayoutParams(width, height)
+        if (binding.frame.layoutParams.height != height) {
+            binding.frame.layoutParams.height = height
             requestLayout()
         }
     }

app/src/main/java/jp/deadend/noname/skk/FlickJPKeyboardView.kt
@@ -44,13 +44,13 @@ class FlickJPKeyboardView(context: Context, attrs: AttributeSet?) : KeyboardView
     private val mCoordinates = IntArray(2)
 
     val mJPKeyboard: Keyboard by lazy {
-        Keyboard(context, R.xml.keys_flick_jp, mService.mScreenWidth, mService.mScreenHeight)
+        Keyboard(context, R.xml.keys_flick_jp, mService.mRootWidth, mService.mScreenHeight)
     }
     val mNumKeyboard: Keyboard by lazy {
-        Keyboard(context, R.xml.keys_flick_number, mService.mScreenWidth, mService.mScreenHeight)
+        Keyboard(context, R.xml.keys_flick_number, mService.mRootWidth, mService.mScreenHeight)
     }
-    private val mVoiceKeyboard: Keyboard by lazy {
-        Keyboard(context, R.xml.keys_flick_voice, mService.mScreenWidth, mService.mScreenHeight)
+    val mVoiceKeyboard: Keyboard by lazy {
+        Keyboard(context, R.xml.keys_flick_voice, mService.mRootWidth, mService.mScreenHeight)
     }
 
     private var mKutoutenLabel = "？\n． ，！\n…"
@@ -228,12 +228,11 @@ class FlickJPKeyboardView(context: Context, attrs: AttributeSet?) : KeyboardView
     internal fun prepareNewKeyboard(
         context: Context,
         widthPixel: Int,
-        heightPixel: Int,
-        bottomPercent: Int
+        heightPixel: Int
     ) {
-        mJPKeyboard.resize(widthPixel, heightPixel, bottomPercent)
-        mNumKeyboard.resize(widthPixel, heightPixel, bottomPercent)
-        mVoiceKeyboard.resize(widthPixel, heightPixel, bottomPercent)
+        mJPKeyboard.resize(widthPixel, heightPixel)
+        mNumKeyboard.resize(widthPixel, heightPixel)
+        mVoiceKeyboard.resize(widthPixel, heightPixel)
         keyboard = mJPKeyboard
         invalidateAllKeys()
 

app/src/main/java/jp/deadend/noname/skk/GodanKeyboardView.kt
@@ -83,7 +83,7 @@ class GodanKeyboardView(context: Context, attrs: AttributeSet?) : KeyboardView(c
         keyboard = Keyboard(
             context,
             if (!mIsASCII && skkPrefs.simpleGodan) R.xml.keys_godan_simple else R.xml.keys_godan,
-            mService.mScreenWidth,
+            mService.mRootWidth,
             mService.mScreenHeight
         )
         setKeyState(mService.engineState)
@@ -148,7 +148,7 @@ class GodanKeyboardView(context: Context, attrs: AttributeSet?) : KeyboardView(c
         val wasASCII = mIsASCII
         mIsASCII = !state.isJapanese
         if (wasASCII != mIsASCII) {
-            prepareNewKeyboard(context, width, height, skkPrefs.keyPaddingBottom)
+            prepareNewKeyboard(context, width, height)
         }
 
         val qKey = checkNotNull(findKeyByCode(keyboard, KEYCODE_GODAN_CHAR_Q)) { "BUG: no Q key" }
@@ -187,18 +187,17 @@ class GodanKeyboardView(context: Context, attrs: AttributeSet?) : KeyboardView(c
     internal fun prepareNewKeyboard(
         context: Context,
         widthPixel: Int,
-        heightPixel: Int,
-        bottomPercent: Int
+        heightPixel: Int
     ) {
         keyboard = Keyboard(
             context,
             if (!mIsASCII && skkPrefs.simpleGodan) R.xml.keys_godan_simple else R.xml.keys_godan,
-            mService.mScreenWidth,
+            mService.mRootWidth,
             mService.mScreenHeight
         )
         keyboard.isShifted = isShifted
         keyboard.isCapsLocked = isCapsLocked
-        keyboard.resize(widthPixel, heightPixel, bottomPercent)
+        keyboard.resize(widthPixel, heightPixel)
         invalidateAllKeys()
 
         readPrefs(context)

app/src/main/java/jp/deadend/noname/skk/Keyboard.kt
@@ -311,9 +311,10 @@ open class Keyboard {
         rows.add(row)
     }
 
-    fun resize(newWidth: Int, newHeight: Int, newBottom: Int) {
-        if ((newWidth == width) && (newHeight == height) && newBottom == bottom) return
+    fun resize(newWidth: Int, newHeight: Int) {
+        if ((newWidth == width) && (newHeight == height)) return
         if (newWidth < 1) return // newHeight はハードキーボード接続時に 0 許容
+        dLog("Keyboard.resize($newWidth, $newHeight) <- ($width, $height)")
 
         var totalHeight = 0
         var maxWidth = 0
@@ -330,7 +331,7 @@ open class Keyboard {
         }
 
         val hScaleFactor = newWidth.toFloat() / maxWidth
-        val vScaleFactor = newHeight.toFloat() * (100 - newBottom) / 100 / totalHeight
+        val vScaleFactor = newHeight.toFloat() / totalHeight
         var x: Int
         var y = 0
         for (row in rows) {
@@ -350,7 +351,6 @@ open class Keyboard {
 
         width = newWidth
         height = newHeight
-        bottom = newBottom
 
         computeNearestNeighbors()
     }

app/src/main/java/jp/deadend/noname/skk/KeyboardView.kt
@@ -361,7 +361,7 @@ open class KeyboardView @JvmOverloads constructor(
 
     public override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
         super.onSizeChanged(w, h, oldw, oldh)
-        mKeyboard.resize(w, h, skkPrefs.keyPaddingBottom)
+        mKeyboard.resize(w, h)
         mBuffer = null
     }
 
@@ -784,7 +784,7 @@ open class KeyboardView @JvmOverloads constructor(
                 }
                 miniKeyboardView.keyboard = Keyboard(
                     context, popupKeyboardId,
-                    mService.mScreenWidth, mService.mScreenHeight,
+                    mService.mRootWidth, mService.mScreenHeight,
                     key.popupCharacters, -1, paddingLeft + paddingRight
                 )
                 miniKeyboardView.setPopupParent(this)

commit ec9628d9cc1f5bb02b524f01872d4314d6abd2ea

Make handleCancel return false if ic==null
d5e92f1c6709dde7916f13f1e00a3d43ee3c1800 broke apps

ime should not return true when hidden

app/src/main/java/jp/deadend/noname/skk/engine/SKKEngine.kt
@@ -209,7 +209,7 @@ class SKKEngine(
     }
 
     internal fun handleCancel(): Boolean =
-        state.handleCancel(this)
+        ic != null && state.handleCancel(this)
 
     /**
      * commitTextのラッパー 登録作業中なら登録内容に追加し，表示を更新