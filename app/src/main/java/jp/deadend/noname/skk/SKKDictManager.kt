tamo/AndroidSKK

commit 9c644f48f7dcb4aef0924026d5b35c1c49adfa69

Close all dictionaries because readonly ones also lock

app/src/main/java/jp/deadend/noname/skk/SKKDictManager.kt
@@ -29,8 +29,10 @@ import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.cancelAndJoin
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.ensureActive
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import kotlinx.coroutines.withTimeoutOrNull
 import java.io.File
 import java.io.FileOutputStream
 import java.io.IOException
@@ -175,6 +177,19 @@ class SKKDictManager : AppCompatActivity() {
         MainScope().launch(Dispatchers.IO) {
             val filePath = filesDir.absolutePath + "/" + item.value
             val previewText = runCatching {
+                if (SKKService.isRunning()) withTimeoutOrNull(500.milliseconds) {
+                    val job = launch {
+                        SKKService.sharedFlow.first { it == SKKService.EVENT_DICT_CLOSING }
+                    } // 先に開始しておく
+                    val intent = Intent(this@SKKDictManager, SKKService::class.java)
+                    intent.putExtra(SKKService.KEY_COMMAND, SKKService.COMMAND_CLOSE_DICT)
+                    startService(intent)
+                    job.join()
+                } ?: run {
+                    SKKLog.e("showPreview error: service not responding")
+                    return@runCatching "Error: service not responding"
+                }
+
                 openDB(filePath, getString(R.string.btree_name), false).use { store ->
                     val sb = StringBuilder()
                     val maxSamples = binding.dictManagerPreviewText.maxLines
@@ -233,18 +248,10 @@ class SKKDictManager : AppCompatActivity() {
         }
 
     override fun onPause() {
-        val dictListString = mDictList
-            .filterNot { it.value.startsWith('/') }
-            .joinToString("") { "${it.key}/${it.value}/" }
-
-        if (skkPrefs.dictOrder != dictListString) {
-            skkPrefs.dictOrder = dictListString
-
-            if (SKKService.isRunning()) { // まだ起動していないなら不要
-                val intent = Intent(this@SKKDictManager, SKKService::class.java)
-                    .putExtra(SKKService.KEY_COMMAND, SKKService.COMMAND_RELOAD_DICT)
-                startService(intent)
-            }
+        if (SKKService.isRunning()) { // まだ起動していないなら不要
+            val intent = Intent(this@SKKDictManager, SKKService::class.java)
+                .putExtra(SKKService.KEY_COMMAND, SKKService.COMMAND_RELOAD_DICT)
+            startService(intent)
         }
 
         super.onPause()

app/src/main/java/jp/deadend/noname/skk/MVStoreStore.kt
@@ -43,7 +43,6 @@ class MVStoreStore(
 
     companion object {
         fun open(path: String, name: String, writable: Boolean = true): MVStoreStore {
-            System.setProperty("h2.fileLockMethod", "NO")
             val builder = MVStore.Builder().fileName(path) //.cacheSize(32)
             if (!writable) builder.readOnly()
             val store = builder.open()

app/src/main/java/jp/deadend/noname/skk/SKKService.kt
@@ -210,10 +210,10 @@ class SKKService : InputMethodService() {
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         SKKLog.d("lifecycle: ${Thread.currentThread().stackTrace[2].methodName}")
         when (intent?.getStringExtra(KEY_COMMAND)) {
-            COMMAND_CLOSE_USER_DICT -> MainScope().launch(Dispatchers.IO) {
-                SKKLog.d("commit user dictionary!")
-                mEngine.closeUserDict()
-                flow.tryEmit(EVENT_USER_DICT_CLOSING)
+            COMMAND_CLOSE_DICT -> MainScope().launch(Dispatchers.IO) {
+                SKKLog.d("closing the dictionaries")
+                mEngine.close()
+                flow.tryEmit(EVENT_DICT_CLOSING)
             }
 
             COMMAND_READ_PREFS -> {
@@ -1702,10 +1702,10 @@ class SKKService : InputMethodService() {
 
         private val flow = MutableSharedFlow<String>(replay = 1, extraBufferCapacity = 1)
         internal val sharedFlow = flow.asSharedFlow()
-        internal const val EVENT_USER_DICT_CLOSING = "jp.deadend.noname.skk.EVENT_USER_DICT_CLOSING"
+        internal const val EVENT_DICT_CLOSING = "jp.deadend.noname.skk.EVENT_DICT_CLOSING"
 
         internal const val KEY_COMMAND = "jp.deadend.noname.skk.KEY_COMMAND"
-        internal const val COMMAND_CLOSE_USER_DICT = "jp.deadend.noname.skk.COMMAND_CLOSE_USER_DICT"
+        internal const val COMMAND_CLOSE_DICT = "jp.deadend.noname.skk.COMMAND_CLOSE_DICT"
         internal const val COMMAND_READ_PREFS = "jp.deadend.noname.skk.COMMAND_READ_PREFS"
         internal const val COMMAND_RELOAD_DICT = "jp.deadend.noname.skk.COMMAND_RELOAD_DICT"
         internal const val COMMAND_MUSHROOM = "jp.deadend.noname.skk.COMMAND_MUSHROOM"

app/src/main/java/jp/deadend/noname/skk/SKKUserDictTool.kt
@@ -436,9 +436,9 @@ class SKKUserDictTool : AppCompatActivity() {
                 mSearchView.queryHint = "辞書をキーボード側で閉じています"
             }
             val job = launch {
-                SKKService.sharedFlow.first { it == SKKService.EVENT_USER_DICT_CLOSING }
+                SKKService.sharedFlow.first { it == SKKService.EVENT_DICT_CLOSING }
             } // 先に開始しておく
-            startServiceCommand(SKKService.COMMAND_CLOSE_USER_DICT)
+            startServiceCommand(SKKService.COMMAND_CLOSE_DICT)
             job.join()
         } ?: run {
             SKKLog.e("openUserDict error: service not responding")

app/src/main/java/jp/deadend/noname/skk/engine/SKKEngine.kt
@@ -138,12 +138,6 @@ class SKKEngine(
         }
     }
 
-    internal fun closeUserDict() {
-        mUserDict.close()
-        mASCIIDict.close()
-        mEmojiDict.close()
-    }
-
     internal fun processKey(keyCode: Int) =
         state.processKey(this, keyCode)
 

commit 4e3c007aed0a2de6d9f30114cd6e6d92146a5427

Fix skkPrefs.dictOrder
regression introduced by 9c644f48f7dcb4aef0924026d5b35c1c49adfa69

app/src/main/java/jp/deadend/noname/skk/SKKDictManager.kt
@@ -240,10 +240,18 @@ class SKKDictManager : AppCompatActivity() {
         }
 
     override fun onPause() {
-        if (SKKService.isRunning()) { // まだ起動していないなら不要
-            val intent = Intent(this@SKKDictManager, SKKService::class.java)
-                .putExtra(SKKService.KEY_COMMAND, SKKService.COMMAND_RELOAD_DICT)
-            startService(intent)
+        val dictListString = mDictList
+            .filterNot { it.value.startsWith('/') }
+            .joinToString("") { "${it.key}/${it.value}/" }
+
+        if (skkPrefs.dictOrder != dictListString) {
+            skkPrefs.dictOrder = dictListString
+
+            if (SKKService.isRunning()) { // まだ起動していないなら不要
+                val intent = Intent(this@SKKDictManager, SKKService::class.java)
+                    .putExtra(SKKService.KEY_COMMAND, SKKService.COMMAND_RELOAD_DICT)
+                startService(intent)
+            }
         }
 
         super.onPause()